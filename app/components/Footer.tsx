afef827a9d64d6e082068658b19116c1a4ca39c8
Roger Montes <rogerxmontes@gmail.com>
2025-10-29 11:39:04 -0600

    añadir componente Footer

---
+++ app/components/Footer.tsx
@@ -0,0 +1,10 @@
+export const Footer = () => {
+  return (
+    <footer className="bg-black text-white text-center py-6 mt-12">
+      <p className="text-sm">
+        © {new Date().getFullYear()} Yessica García Fotografía. Todos los
+        derechos reservados.
+      </p>
+    </footer>
+  );
+};
